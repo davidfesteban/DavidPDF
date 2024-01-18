c21c5f2860ef8f0953e668d83137cf2a2b710dcd
mark.shmarov <shmarov.mark@gmail.com>
2024-01-18 12:29:35 +0100

    added constants class

---
+++ src/main/java/dev/misei/html2pdf/model/Constants.java
@@ -0,0 +1,10 @@
+package dev.misei.html2pdf.model;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public static final String SELENIARM = "http://seleniarm:4444";
+    public static final int WEBDRIVER_WAIT_TIMEOUT_IN_SECONDS = 5;
+}
