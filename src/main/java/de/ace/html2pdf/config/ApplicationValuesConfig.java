2086e085dc68b38f760137f67e062a3dba14ce01
mark.shmarov <shmarov.mark@gmail.com>
2024-01-23 13:10:34 +0100

    refactor + fixes

---
+++ src/main/java/de/ace/html2pdf/config/ApplicationValuesConfig.java
@@ -0,0 +1,20 @@
+package de.ace.html2pdf.config;
+
+import de.ace.html2pdf.model.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationValuesConfig {
+
+    @Autowired
+    public void setApiKey(@Value("${security.api.key}") String apiKey) {
+        Constants.setApiKey(apiKey);
+    }
+
+    @Autowired
+    public void setWebDriverPath(@Value("${selenium-web-driver-path}") String path) {
+        Constants.setWebDriverPath(path);
+    }
+}
